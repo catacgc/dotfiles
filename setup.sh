809ac461c42abd2059ac5735bcde0dba1813fa91
Catalin Costache <catacgc@gmail.com>
2012-07-23 11:41:28 +0300

    finish setup.sh script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env sh
 
+target="~/.vim`date +F'%Y%m%dw'`"
+mkdir -p $target
+mv ~/.vim $target
+mv ~/.vimrc $target
+
+ln -s `pwd`/.vimrc ~/
+ln -s `pwd`/.vim ~/
+
 git clone https://github.com/gmarik/vundle.git .vim/bundle/vundle
 vim +BundleInstall +quitall
