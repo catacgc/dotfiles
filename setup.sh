c6686a36cc7189a9012939cc2823827fff497fce
Catalin Costache <catacgc@gmail.com>
2014-04-05 15:28:00 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,21 @@
-#!/usr/bin/env sh
+BASEDIR=~/.env
+VIRTUAL_ENV_VERSION=1.9
 
-easy_install sh
+mkdir -p $BASEDIR
+pushd $BASEDIR
+
+# download venv
+curl -sO https://pypi.python.org/packages/source/v/virtualenv/virtualenv-$VIRTUAL_ENV_VERSION.tar.gz
+tar xvfz virtualenv-$VIRTUAL_ENV_VERSION.tar.gz
+
+# create default local python env
+deactivate
+pushd virtualenv-$VIRTUAL_ENV_VERSION
+./virtualenv.py ~/$BASEDIR/env
+source ~/$BASEDIR/env/bin/activate
+popd
+
+# install some basic pip packages
+pip install sh
+
+# start heavy lifting setup
