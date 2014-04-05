4d9d336b6f85ee4527d1af6437cb9e3c35b3abdc
Catalin Costache <catacgc@gmail.com>
2014-04-05 17:02:44 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,47 @@
+from sh import curl, ln, rm
+import shutil
+import os
+
+def describe(f):
+    def _d(*args, **kwargs):
+        print "%s %s %s" % (f.__name__.capitalize(), ", ".join([str(arg) for arg in args]), kwargs)
+        return f(*args, **kwargs)
+    return _d
+
+def furl(f):
+    return 'https://raw.githubusercontent.com/catacgc/dotfiles/master/%s' % f
+
+def fullpath(f):
+    return os.path.realpath(os.path.expanduser(f))
+
+@describe
+def download(f):
+    curl(furl(f), "-sO")
+
+def copy(source, dest):
+    describe(shutil.copy)(fullpath(source), fullpath(dest))
+
+@describe
+def write_config(fname, config, label, sep=' ### '):
+    result = []
+    with open(fullpath(fname), 'r+') as f:
+        for line in f.readlines():
+            if line.find(sep + label) == -1:
+                result.append(line)
+
+        config  = [line + sep + label + "\n" for line in config]
+        result.extend(config)
+        f.seek(0)
+        f.writelines(result)
+
+if __name__ == '__main__':
+    # vimrc
+    download(".vimrc")
+    copy(".vimrc", "~/.vimrc")
+
+    # path settings
+    config = [
+      "source " + fullpath("./env/bin/activate")
+    ]
+    write_config("~/.bashrc", config, "Auto setup-up. DONT't change")
+    
